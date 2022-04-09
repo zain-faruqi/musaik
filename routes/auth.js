d15782ded9e8a401788f3ba53dee6bab3e8714e4
Zain Faruqi <zf698@nyu.edu>
2022-04-09 14:29:26 -0400

    fixed build

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../models/user');
 const Joi = require('joi');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 router.post('/', async (req, res) => {
     try {
